946328f6ec41229eb6dc215cfa65d48a4b03c2b3
horkar667 <jmmpp413@yahoo.co.jp>
2021-11-20 16:42:58 +0900

    [Add]条件分岐(if文)

---
+++ Lesson10_01.java
@@ -0,0 +1,14 @@
+class Lesson10_01 {
+  public static void main(String[] args) {
+    // 条件分岐（if文）
+    int age = 22;
+
+    if (age >= 20) {
+      System.out.println("adult");
+    } else if (age == 0) {
+      System.out.println("baby");
+    } else {
+      System.out.println("child");
+    }
+  }
+}
