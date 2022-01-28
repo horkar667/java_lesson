bc3d6ded8408ab1b94c3fc22d4d4030810b45710
horkar667 <jmmpp413@yahoo.co.jp>
2022-01-28 17:15:59 +0900

    [Add]型変換

--- Lesson06_06.java
+++ Lesson06_06.java
@@ -1,17 +1,17 @@
 public class Lesson06_06 {
   public static void main(String[] args) {
-    String x = "10";
+    String x = "100";
     Integer y = Integer.parseInt(x); //文字列をint型にするメソッド
     boolean result1 = y instanceof Integer;
     System.out.println(result1);
 
-    int a = 10;
+    int a = 100;
     String b = String.valueOf(a); //整数をString型にするメソッド
     boolean result2 = b instanceof String;
     System.out.println(result2);
 
     // キャスト
-    long c = 20L;
+    long c = 200L;
     Integer d = (int)c; //キャスト演算子を行うと一時的な型変換が可能
     boolean result3 = d instanceof Integer;
     System.out.println(result3);
